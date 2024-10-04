0c4c6b3e962fca21a091df44a5f9f049a77bea7b
PlayQuartz <57173794+PlayQuartz@users.noreply.github.com>
2024-10-04 13:33:47 +0200

    Updated build for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
-import './index.css';
 import React from 'react';
-import { createRoot } from 'react-dom/client';  
-import { BrowserRouter } from 'react-router-dom';  
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import RankRumble from './RankRumble';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <RankRumble />
-    </BrowserRouter>
+    <RankRumble />
   </React.StrictMode>
 );
